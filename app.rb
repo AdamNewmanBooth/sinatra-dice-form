74f5dd2fe836c9f59f3ae05fdbb46d6a42ab9897
AdamNewmanBooth <164922100+AdamNewmanBooth@users.noreply.github.com>
2024-04-17 01:57:54 +0000

    commit1

--- app.rb
+++ app.rb
@@ -3,7 +3,9 @@ require "sinatra/reloader"
 
 get("/") do
   "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+<h1>Dice Roll</h1>"
+end
+
+get("/process_roll") do
+    erb(:results)
 end
