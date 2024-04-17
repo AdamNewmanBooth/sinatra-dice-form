3e23fbb87576a2381277b1f25ccb794ff5dcca67
AdamNewmanBooth <164922100+AdamNewmanBooth@users.noreply.github.com>
2024-04-17 14:03:37 +0000

    Commit3

--- app.rb
+++ app.rb
@@ -2,12 +2,18 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-<h1>Dice Roll</h1>"
+  erb(:homepage)
 end
 
 get("/process_roll") do
    @num_dice=params.fetch("dice").to_i
-  @numsides=params.fetch("sides").to_i
+  @num_sides=params.fetch("sides").to_i
+
+  @rolls = []
+
+  @num_dice.times do
+    @rolls.push(rand(1..@num_sides))
+  end 
+
   erb(:results)
 end
